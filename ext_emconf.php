f20c631d95077f67554245e091eee45246ce83b4
Felix Nagel <info@felixnagel.com>
2016-04-07 00:19:45 +0200

    [TASK] Add PHP dependency constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,6 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.1-dev',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.4.0-7.0.99',
 			'typo3' => '6.2.0-7.6.99',
 			'scheduler' => '',
 		),
