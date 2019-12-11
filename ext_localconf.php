190d344430d29deec0dc81a158147c68d8714e81
Felix Nagel <info@felixnagel.com>
2019-12-11 19:01:42 +0100

    [TASK] Remove deprecated use of $_EXTKEY variable
    Needed for TYPO3 10.x.
    
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.0/Important-82692-GuidelinesForExtensionFiles.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,8 +5,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['FelixNagel\\PrivacyGuard\\Task\\PrivacyGuardTask'] = [
-    'extension' => $_EXTKEY,
-    'title' => 'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xml:localconf_title',
-    'description' => 'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xml:localconf_description',
+    'extension' => 'privacyguard',
+    'title' => 'LLL:EXT:privacyguard/Resources/Private/Language/locallang.xml:localconf_title',
+    'description' => 'LLL:EXT:privacyguard/Resources/Private/Language/locallang.xml:localconf_description',
     'additionalFields' => 'FelixNagel\\PrivacyGuard\\Task\\PrivacyGuardAdditionalFieldProvider',
 ];
